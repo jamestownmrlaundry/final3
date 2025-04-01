6bd4de223cba9f8d069f2017f4023883ff4a8a23
jamestownmrlaundry <jamestownmrlaundry@gmail.com>
2025-04-01 21:06:51 +0530

    Update footerOne.js

--- components/data/footerOne.js
+++ components/data/footerOne.js
@@ -1,7 +1,7 @@
 const footerOne = {
     logo: '/assets/img/logo-9.png',
     title_1: 'About Us',
-    description: 'Subscribe to out newsletter today to receive updates on the latest news, releases and special offers respect your privacy.',
+    description: 'Subscribe to our newsletter today to receive updates on the latest news, releases, and special offers. We respect your privacy.',
     title_2: 'Other Pages',
     widgetMenus: [
         {link:'/about',title:'About Us'},
@@ -11,12 +11,24 @@ const footerOne = {
     ],
     title_3: 'Gallery',
     title_4: 'Office Info',
-    office_des: '1234 Lake Tahoe Blvd, South Lake Tahoe , California USA',
+    office_des: '1234 Lake Tahoe Blvd, South Lake Tahoe, California USA',
     officeInfos: [
-        {info:'JamestownJunction,Anjugramam,kaniyakumari(dist)-629401',icon: <i className="fal fa-map-marked-alt"></i>},
-        {link:'mailto:mrlaundryjamestown@gmail.com',info:'mrlaundryjamestown@gmail.com',icon: <i className="fal fal fa-envelope-open-text"></i>},
-        {link:'tel:+125(895)658568',info:'6385856384',icon: <i className='fal fa-phone-alt'></i>}
+        {
+            link: 'https://www.google.com/maps?q=8.1486944,77.5571944',
+            info: <>Jamestown junction Anjugramam <br /> kaniyakumari(Dist)-629401</>,
+            icon: <i className="fal fa-map-marked-alt"></i>
+        },
+        {
+            link: 'mailto:Vinithvini725@gmail.com',
+            info: 'Vinithvini725@gmail.com',
+            icon: <i className="fal fa-envelope-open-text"></i>
+        },
+        {
+            link: 'tel:+125(895)658568',
+            info: '6385856384',
+            icon: <i className='fal fa-phone-alt'></i>
+        }
     ],
-  }
+}
 
-  export default footerOne;
+export default footerOne;
